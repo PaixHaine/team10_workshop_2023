8f04882bc434a5a26d8ad7fa6d49b71062ac4a2d
PaixHaine <pnzinsou@gmail.com>
2023-03-30 12:41:13 +0200

    Correction erreur routes

--- nom-du-projet/routes/web.php
+++ nom-du-projet/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LeadController;
 use App\Http\Controllers\ProspectController;
 use Illuminate\Support\Facades\Auth;
@@ -26,12 +25,42 @@ Route::get('/', function () {
 });
 
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/leads', [LeadController::class]);
-Route::get('/prospects', [ProspectController::class]);
-Route::get('/clients', [ClientController::class]);
+// Route pour la page de listing et gestion des leads
+Route::middleware(['auth'])->group(function () {
+    Route::get('/leads', [LeadController::class, 'index'])->name('leads.index');
+    Route::get('/leads/create', [LeadController::class, 'create'])->name('leads.create');
+    Route::post('/leads', [LeadController::class, 'store'])->name('leads.store');
+    Route::get('/leads/{lead}', [LeadController::class, 'show'])->name('leads.show');
+    Route::get('/leads/{lead}/edit', [LeadController::class, 'edit'])->name('leads.edit');
+    Route::put('/leads/{lead}', [LeadController::class, 'update'])->name('leads.update');
+    Route::delete('/leads/{lead}', [LeadController::class, 'destroy'])->name('leads.destroy');
+});
+
+// Route pour la page de listing et gestion des prospects
+Route::middleware(['auth'])->group(function () {
+    Route::get('/prospects', [ProspectController::class, 'index'])->name('prospects.index');
+    Route::get('/prospects/create', [ProspectController::class, 'create'])->name('prospects.create');
+    Route::post('/prospects', [ProspectController::class, 'store'])->name('prospects.store');
+    Route::get('/prospects/{prospect}', [ProspectController::class, 'show'])->name('prospects.show');
+    Route::get('/prospects/{prospect}/edit', [ProspectController::class, 'edit'])->name('prospects.edit');
+    Route::put('/prospects/{prospect}', [ProspectController::class, 'update'])->name('prospects.update');
+    Route::delete('/prospects/{prospect}', [ProspectController::class, 'destroy'])->name('prospects.destroy');
+});
+
+// Route pour la page de listing et gestion des clients
+Route::middleware(['auth'])->group(function () {
+    Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+    Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+    Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+    Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
+    Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+    Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+});
+
 
 Auth::routes();
+//Route::get('/home', [HomeController::class,'index'])->name('home');
 
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
